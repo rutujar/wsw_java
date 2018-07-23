c3025627ae2c0777f4185dccb626376a32a91d92
rutujar <rutujarajesh23@gmail.com>
2018-07-23 12:39:17 +0530

    Create wiper.java

---
+++ wiper.java
@@ -0,0 +1,18 @@
+public class wiper {
+// State variables.
+private int wiperSpeed;
+
+public wiper(int wiperSpeed) {
+this.wiperSpeed = wiperSpeed;
+
+}
+// Get the current wiper speed.
+public int getWiperSpeed() {
+return wiperSpeed;
+}
+// Set the wiper speed.
+public void setWiperSpeed(int wiperSpeed) {
+this.wiperSpeed = wiperSpeed;
+}
+
+}
