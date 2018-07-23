ea3d2302fdd93a3a1104cc8bafd707b27f0075dc
rutujar <rutujarajesh23@gmail.com>
2018-07-23 11:48:08 +0530

    Create SimpleCalculator.java

---
+++ SimpleCalculator.java
@@ -0,0 +1,17 @@
+public class SimpleCalculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	public int div(int a, int b) {
+		return a / b;
+	}
+	public static final void main(String[] args) {
+		System.out.println("Hello world?");
+	}
+}
