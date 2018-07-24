ff336b5ad4a535e8a71e3980e2244d268fda1e2e
rutujar <rutujarajesh23@gmail.com>
2018-07-24 10:44:20 +0530

    Update windshieldWiper.java

--- windshieldWiper.java
+++ windshieldWiper.java
@@ -1,5 +1,5 @@
 public class windshieldWiper {
-	
+	public static void main(String[] args){
 	private lever l;
 	private dial d;
 	private wiper w;
@@ -187,5 +187,5 @@ public class windshieldWiper {
 	}
 	}
 	}
-	
+	}
 }
