0d64defd2d61e5548504aba3af40cbfc3db78fea
rutujar <rutujarajesh23@gmail.com>
2018-07-28 15:58:00 +0530

    Create dial.java

---
+++ src/main/java/dial.java
@@ -0,0 +1,17 @@
+public class dial {
+
+	private int dialPosition;
+	
+	public dial(int dPos)
+	{
+		this.dialPosition=dPos;
+	}
+	
+	public int getDialPosition() {
+		return dialPosition;
+		}
+		// Set the dial position.
+		public void setDialPosition(int dialPosition) {
+		this.dialPosition = dialPosition;
+		}
+}
