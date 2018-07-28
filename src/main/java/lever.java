cfdd72964f6d787eefcb234f551903b002185323
rutujar <rutujarajesh23@gmail.com>
2018-07-28 15:58:23 +0530

    Create lever.java

---
+++ src/main/java/lever.java
@@ -0,0 +1,21 @@
+public class lever {
+
+	private String leverPosition;
+	
+	public lever(String lev)
+	{
+		this.leverPosition=lev;
+	}
+	
+	// Get the current position of the lever.
+	public String getLeverPosition() {
+	return leverPosition;
+	}
+	// Set the lever position.
+	public void setLeverPosition(String leverPosition) {
+	this.leverPosition = leverPosition;
+	}
+	
+	
+		
+}
