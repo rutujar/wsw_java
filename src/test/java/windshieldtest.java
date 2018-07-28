6636ea2bc5ddedc8b89f5d4363928ee18a1966a6
rutujar <rutujarajesh23@gmail.com>
2018-07-28 16:05:51 +0530

    Update windshieldtest.java

--- src/test/java/windshieldtest.java
+++ src/test/java/windshieldtest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
